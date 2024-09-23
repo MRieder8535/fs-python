841a8d446f4b69191e905045932c2a455022a68e
Mitchell Rieder <mar8535@rit.edu>
2024-09-23 13:33:49 -0400

    Created Assignment3.py and completed assignment

---
+++ Assignments/Assignment1.py
@@ -0,0 +1,9 @@
+"""
+FIRST ASSIGNMENT FILE
+"""
+
+
+streetName = "Old English"
+streetChars = len(streetName)
+sentence = f"I live on {streetName}. It is {streetChars} Characters Long"
+print(sentence)
